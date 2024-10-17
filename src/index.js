b0afc0031b43faa2d01a102c950ab5cab727550a
JS <chjwest01@gmail.com>
2024-10-17 20:39:10 +0900

    Data Loader 작성완료

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import * as THREE from 'three';
 import House from './house.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 var scene;
 var camera;
 var renderer;
 var controls;
+var loader = new GLTFLoader(); // 3D data loader
 
 function init(){
     scene = new THREE.Scene();
@@ -15,6 +17,8 @@ function init(){
         0.1, // 가까운 절단면
         1000 // 먼 절단면
     );
+
+    DataLoader('data/kirby_pixar_3d.glb', 'kirby Model');
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -35,7 +39,7 @@ function init(){
 
     // 집
     const house = new House(scene, 50);
-    house.init();
+    // house.init();
 }
 
 function animate() {
@@ -43,6 +47,33 @@ function animate() {
     controls.update(); // OrbitControls 업데이트
     renderer.render(scene, camera);
 }
+
+/**
+ * Data 파일을 로드하고 씬에 추가하기 위한 함수입니다.
+ * GLB 파일을 권장합니다.
+ * @param {string} path 로드할 파일의 경로. (/data/[파일이름] 형식으로 작성합니다.)
+ * @param {string} fileName 로드한 파일의 이름. console에 찍어서 데이터 형식을 확인하려고 사용함.
+ * @returns {null} 리턴은 없는데 객체 형태로 내보내고 싶어요.. 근데 안됨
+ * @example // 사용 예
+ * DataLoader('data/kirby_pixar_3d.glb', '커비')
+ * // 콘솔 결과
+ * 커비 {scene: Group, scenes: Array(1), animations: Array(1), cameras: Array(0), asset: {…}, …}
+ */
+function DataLoader(path, fileName){
+    loader.load(
+        path, // 3D data 경로.
+        function (gltf) { // Data 불러오는 함수
+            console.log(fileName, gltf);
+            const characterMesh = gltf;
+            scene.add(gltf.scene);
+        },
+        undefined, 
+        function (error) {// 실패 시 에러 출력
+            console.error(error);
+        }
+    );
+}
+
 
 init();
 animate();
