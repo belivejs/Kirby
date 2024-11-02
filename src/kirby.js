14f0ecf782ac1e5f2aedfa4162fc1c827a4e1bfb
hyeonji91 <112065014+hyeonji91@users.noreply.github.com>
2024-11-02 21:52:54 +0900

    :sparkles: Feat : 커비 이동

--- src/kirby.js
+++ src/kirby.js
@@ -19,13 +19,14 @@ class Kirby{
 
         this._setupModel();
         this._setupControl();
+
         
         requestAnimationFrame(this.render.bind(this));
     }
 
     //커비 모델 불러오고 애니메이션 세팅
     _setupModel(){
-        new GLTFLoader().load('data/kirby_base.glb', (gltf) => {
+        new GLTFLoader().load('data/kirby_dirty_face.glb', (gltf) => {
             var scale = 0.2;
             
             //커비 에셋을 scene에 등록
@@ -48,7 +49,8 @@ class Kirby{
             this._currentAnimationAction = null;
 
             this._model = model;  
-            
+            console.log("model position: ", this._model.position.x,":", this._model.position.y)
+
 
 
         })
@@ -99,6 +101,39 @@ class Kirby{
         }
     }
 
+    //방향별로 offset을 줘서 캐릭터가 이동하는 방향을 바라보도록 하기
+    _directionOffset(){
+        const pressedKeys = this._pressedKeys;
+        let directionOffset = 0; //기본 : 정면
+
+        //정면에서 시계 방향 -, 반시계 방향 +. 최대 180
+        if(pressedKeys['w']==true){
+            if (pressedKeys['a']) {
+                directionOffset = Math.PI / 4 // w+a (45도)
+            } else if (pressedKeys['d']) {
+                directionOffset = - Math.PI / 4 // w+d (-45도)
+            }
+        } else if(pressedKeys['s'] == true){
+            if (pressedKeys['a']) {
+                directionOffset = Math.PI / 4 + Math.PI / 2 // s+a (135도)
+            } else if (pressedKeys['d']) {
+                directionOffset = -Math.PI / 4 - Math.PI / 2 // s+d (-135도)
+            } else {
+                directionOffset = Math.PI // s (180도)
+            }
+        } else if (pressedKeys['a']) {
+            directionOffset = Math.PI / 2 // a (90도)
+        } else if (pressedKeys['d']) {
+            directionOffset = - Math.PI / 2 // d (-90도)
+        }
+
+        return directionOffset;        
+    }
+
+    _speed = 0;
+    _maxSpeed = 0;
+    _acceleration = 0;
+
     update(time){
         time *= 0.001; // second unit
 
@@ -107,28 +142,47 @@ class Kirby{
             const deltaTime = time - this._previousTime;
             this._mixer.update(deltaTime);
 
+
+            /// 카메라가 보는 방향으로 캐릭터 회전 ///
             //카메라가 보는 방향을 캐릭터도 바라보게하기위한 angle값, Math.PI는 캐릭터가 기본적으로 바라보고 있는 값
             const angleCameraDirectionAxisY = Math.atan2(
                 (this._camera.position.x - this._model.position.x),
                 (this._camera.position.z - this._model.position.z)
             ) + Math.PI;
-
             //캐릭터 회전
             const rotateQuarternion = new THREE.Quaternion();
             rotateQuarternion.setFromAxisAngle(
-                new THREE.Vector3(0,1,0), //y축 방향으로
-                angleCameraDirectionAxisY //angle만큼 
+                new THREE.Vector3(0,1,0), //y축 방향으로 -> y축 고정으로 돌린단 말
+                angleCameraDirectionAxisY + this._directionOffset() //이 angle만큼 
             );
             this._model.quaternion.rotateTowards(
                 rotateQuarternion, 
                 THREE.MathUtils.degToRad(5) //목표angle에 도달할때까지 5도씩 점진적으로 회전한다.
             );
 
-            // 현재 속도에 맞춰 이동
-            if (this._speed > 0) {
-                this._model.position.x += this._speed * deltaTime;
-                this._model.position.z += this._speed * deltaTime;
-            }
+
+
+            /// 캐릭터 걷기 ///
+            //카메라가 바라보는 방향 가져오기
+            const walkDirection = new THREE.Vector3();
+            this._camera.getWorldDirection(walkDirection);
+            walkDirection.y = 0; //위아래 이동x
+            walkDirection.normalize();
+            walkDirection.applyAxisAngle(new THREE.Vector3(0,1,0), this._directionOffset()); //누른 방향으로 보정
+            //최대 속도 설정
+            if(this._speed < this._maxSpeed) 
+                this._speed += this._acceleration;
+            else 
+                this._speed -= this._acceleration*2;
+            // 방향 속도 합치기
+            const moveX = walkDirection.x * (this._speed * deltaTime);
+            const moveZ = walkDirection.z * (this._speed * deltaTime);
+           // 이동
+            this._model.position.x += moveX;
+            this._model.position.z += moveZ; 
+
+
+
         }
         this._previousTime = time;
     }
