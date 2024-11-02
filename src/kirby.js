53039eb22e98f9b4705d59bb36a4f593a53fe136
hyeonji91 <112065014+hyeonji91@users.noreply.github.com>
2024-11-02 21:17:23 +0900

    :sparkles: Feat : 카메라가 바라보는 방향으로 캐릭터도 바라봄

--- src/kirby.js
+++ src/kirby.js
@@ -46,7 +46,6 @@ class Kirby{
             this._mixer = mixer;
             this._animationMap = animationsMap;
             this._currentAnimationAction = null;
-            // this._currentAnimationAction.play();
 
             this._model = model;  
             
@@ -58,11 +57,12 @@ class Kirby{
     _setupControl(){
         this._pressedKeys = {};
         
+        //키보드 클릭 이벤트 - 걷기
         document.addEventListener('keydown', (event) => {
             this._pressedKeys[event.key.toLowerCase()] = true;
             this._processAnimation();
         });
-
+        //키보드 뗄때 이벤트 - 멈추기
         document.addEventListener('keyup', (event) => {
             this._pressedKeys[event.key.toLowerCase()] = false;
             this._processAnimation();
@@ -74,23 +74,23 @@ class Kirby{
 
         if(this._pressedKeys["w"] || this._pressedKeys["a"] || this._pressedKeys["s"] || this._pressedKeys["d"]){
             this._currentAnimationAction = this._animationMap["walk"];      
-            this._speed = 2;
+            this._maxSpeed = 40;
+            this._acceleration = 3;
         } else {
             this._currentAnimationAction = null;
             this._speed = 0;
+            this._maxSpeed = 0;
+            this._acceleration = 0;
         }
         
 
         //애니메이션 전환시 부드럽게
         if(previousAnimationAction == null && this._currentAnimationAction == null){
-            console.log('1')   
         } 
         else if(previousAnimationAction == null){
-            console.log("2")   
             this._currentAnimationAction.reset().fadeIn(0.5).play();
         } 
         else if (this._currentAnimationAction == null){
-            console.log("3")   
             previousAnimationAction.fadeOut(0.5);
         } 
         else if (previousAnimationAction !== this._currentAnimationAction) {
@@ -106,6 +106,23 @@ class Kirby{
         if(this._mixer){
             const deltaTime = time - this._previousTime;
             this._mixer.update(deltaTime);
+
+            //카메라가 보는 방향을 캐릭터도 바라보게하기위한 angle값, Math.PI는 캐릭터가 기본적으로 바라보고 있는 값
+            const angleCameraDirectionAxisY = Math.atan2(
+                (this._camera.position.x - this._model.position.x),
+                (this._camera.position.z - this._model.position.z)
+            ) + Math.PI;
+
+            //캐릭터 회전
+            const rotateQuarternion = new THREE.Quaternion();
+            rotateQuarternion.setFromAxisAngle(
+                new THREE.Vector3(0,1,0), //y축 방향으로
+                angleCameraDirectionAxisY //angle만큼 
+            );
+            this._model.quaternion.rotateTowards(
+                rotateQuarternion, 
+                THREE.MathUtils.degToRad(5) //목표angle에 도달할때까지 5도씩 점진적으로 회전한다.
+            );
 
             // 현재 속도에 맞춰 이동
             if (this._speed > 0) {
